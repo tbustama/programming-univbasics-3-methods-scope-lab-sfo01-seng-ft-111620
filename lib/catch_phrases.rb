008b664186dc4a0463e9860ecd72b30d481903da
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-02 20:02:50 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,9 +2,8 @@ def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
